8956c28003b6bd7bbd154348148a7849d0d22d1d
Faten-pc <Faten-Orady>
2021-06-29 12:58:32 +0200

    water

--- app/src/main/java/com/example/waterremembering/MainActivity.java
+++ app/src/main/java/com/example/waterremembering/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.waterremembering;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.logging.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        Thread thread =new Thread(){
+            public void run(){
+
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        };
+        thread.start();
+
+}}
